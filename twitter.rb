0c9f95b918497be79cd4f4e798dfd2098964aab6
Kunio Murasawa <kunio.murasawa@gmail.com>
2009-10-25 23:20:00 +0900

    refactoring

--- twitter.rb
+++ twitter.rb
@@ -7,43 +7,34 @@
 require 'net/https'
 require 'rexml/document'
 
-HOST = "twitter.com"
-GET = "GET"
-POST = "POST"
-
 class Twitter
-  attr_reader :user_id, :users, :res
+  HOST = "twitter.com"
+  HTTP_PORT = 80
+  HTTPS_PORT = 443
+  GET = :get
+  POST = :post
 
-  def initialize(user = nil, pass = nil, ssl = false)
+  attr_reader :user_id, :users
+
+  def initialize(user, pass, enable_ssl = false)
     @user = user
     @pass = pass
-    @ssl = ssl
+    @enable_ssl = enable_ssl
 
-    @port = (ssl == true) ? 443 : 80
+    @port = (enable_ssl == true) ? HTTPS_PORT : HTTP_PORT
     @users = Hash.new
   end
 
   # ログイン (verify_credentials)
-  def login(user = nil, pass = nil)
+  def login()
     path = "/account/verify_credentials.xml"
 
-    @user = user if user != nil
-    @pass = pass if pass != nil
-
-    req = request(GET, path)
-    res = http.start { |h|
-      h.request(req)
-    }
-
-    @res = res
-    if res.class != Net::HTTPOK
-      return
-    end
+    res = request(GET, path)
 
     doc = REXML::Document.new(res.body)
-    doc.elements.each('user') { |u|
-      my = User.parse(u)
-      @user_id = my.user_id
+    doc.elements.each('user') { |user|
+      my = User.parse(user)
+      @user_id = my.id
       @users[@user_id] = my if @users[@user_id] == nil
     }
 
@@ -51,21 +42,14 @@ class Twitter
     @cookie = res.get_fields('set-cookie')
   end
 
-  # つぶやく (update) 
-  def update(msg)
+  # アップデート (つぶやく)
+  def update(message)
     path = "/statuses/update.xml"
     param = "status="
 
-    return if msg.length > 140
+    return if message.length > 140  # 例外の方がいいかな？
 
-
-    req = request(POST, path)
-    req.body = param + msg
-    res = http.start { |h|
-      h.request(req)
-    }
-
-    @res = res
+    res = request(POST, path, param + message)
   end
 
   # friends timeline
@@ -75,50 +59,46 @@ class Twitter
 
     path += param + page.to_s if page != nil && page > 0
 
-    req = request(GET, path)
-    res = http.start { |h|
-      h.request(req)
-    }
-
-    @res = res
-    if res.class != Net::HTTPOK
-      return nil
-    end
+    res = request(GET, path)
 
     statuses = []
     doc = REXML::Document.new(res.body)
-    doc.elements.each('statuses/status') { |s|
-      u = User.parse(s.elements['user'])
-      @users[u.user_id] = u if @users[u.user_id] == nil
-      statuses << Status.parse(s, u.user_id)
+    doc.elements.each('statuses/status') { |status|
+      user = User.parse(status.elements['user'])
+      @users[user.id] = user if @users[user.id] == nil
+      statuses << Status.parse(status, user.id)
     }
  
     return statuses
   end
 
-  def request(method, path)
+  def request(method, path, body = nil)
     case method
-    when GET
+    when :get
       req = Net::HTTP::Get.new(path)
-    when POST
+    when :post
       req = Net::HTTP::Post.new(path)
     else
-      return nil
+      return nil # 例外の方がいいかな？
     end
     req.basic_auth(@user, @pass)
+    req.body = body if body != nil
 
-    return req
+    http = Net::HTTP.new(HOST, @port)
+    http.use_ssl = @enable_ssl
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE if @enable_ssl == true # とりあえず未チェック
+
+    response = http.start { |h|
+      h.request(req)
+    }
+
+    if response.class != Net::HTTPOK
+      return nil # 例外の方がいいかな？
+    end
+
+    return response
   end
   private :request
-
-  def http
-    h = Net::HTTP.new(HOST, @port)
-    h.use_ssl = @ssl
-    h.verify_mode = OpenSSL::SSL::VERIFY_NONE if @ssl == true # とりあえず未チェック
-
-    return h
-  end
-  private :http
 
   # ステータス情報
   #  まだ一部の情報しか保持していない
@@ -126,47 +106,49 @@ class Twitter
     attr_reader :created_at, :status_id, :text
     attr_accessor :user_id
 
-    def initialize(sid, create, text, uid = nil)
-      @created_at = create
-      @status_id = sid
+    def initialize(id, created_at, text, user_id = nil)
+      @id = id
+      @created_at = created_at
       @text = text
-      @user_id = uid
+      @user_id = user_id
     end
 
-    def self.parse(msg, uid = nil)
-      sid = msg.elements['id'].text
-      create = msg.elements['created_at'].text
-      text = msg.elements['text'].text
+    def self.parse(message, user_id)
+      elements = message.elements
+      id = elements['id'].text
+      create = elements['created_at'].text
+      text = elements['text'].text
 
-      return Status.new(sid, create, text, uid)
+      return Status.new(id, create, text, user_id)
     end
   end
 
   # ユーザ情報
   #  まだ一部の情報しか保持していない
   class User
-    attr_reader :user_id, :name, :screen_name, :location, :description, :image_url, :url
+    attr_reader :id, :name, :screen_name, :location, :description, :image_url, :url
  
-    def initialize(uid, name, sname, loc, desc, image, url)
-      @user_id = uid
+    def initialize(id, name, screen_name, location, description, image_url, url)
+      @id = id
       @name = name
-      @screen_name = sname
-      @location = loc
-      @description = desc
-      @image_url = image
+      @screen_name = screen_name
+      @location = location
+      @description = description
+      @image_url = image_url
       @url = url
     end
 
-    def self.parse(msg)
-      uid = msg.elements['id'].text
-      name = msg.elements['name'].text
-      sname = msg.elements['screen_name'].text
-      loc = msg.elements['location'].text
-      desc = msg.elements['description'].text
-      image = msg.elements['profile_image_url'].text
-      url = msg.elements['url'].text
+    def self.parse(message)
+      elements = message.elements
+      id = elements['id'].text
+      name = elements['name'].text
+      sname = elements['screen_name'].text
+      loc = elements['location'].text
+      desc = elements['description'].text
+      image = elements['profile_image_url'].text
+      url = elements['url'].text
 
-      return User.new(uid, name, sname, loc, desc, image, url)
+      return User.new(id, name, sname, loc, desc, image, url)
     end
   end
 end
